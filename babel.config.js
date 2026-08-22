fd2499b637e4ffb58e1afbf22fbf09c8cfc5ec36
koalasam <koalasamprice@gmail.com>
2026-08-22 18:15:30 +1200

    updated packages

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+    // babel-preset-expo automatically configures the Reanimated/Worklets
+    // Babel plugins when react-native-reanimated is installed, so they
+    // no longer need to be (and must not be) added manually below.
     presets: ["babel-preset-expo"],
-    plugins: [
-      // Reanimated plugin has to be listed last
-      "react-native-reanimated/plugin",
-    ],
+    plugins: [],
   };
 };
